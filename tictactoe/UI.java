69251c644b64ebe4b564c5f9ddd2b725b76b27a4
Roger Jaffe <rogerjaffe@gmail.com>
2021-06-26 13:35:56 -0700

    Debug 4 - fix printBoard

--- tictactoe/UI.java
+++ tictactoe/UI.java
@@ -15,7 +15,13 @@ public class UI
 
     // Utility methods
     public String getXOrO(int whoseMove) {
-        return (whoseMove == -1) ? "X" : "O";
+        if (whoseMove == -1) {
+            return "X";
+        } else if (whoseMove == 1) {
+            return "O";
+        } else {
+            return " ";
+        }
     }
 
     public String getPlayerName(int whoseMove, String xName, String yName) {
@@ -75,6 +81,7 @@ public class UI
         System.out.println(Constants.DIVIDER_STRING);
         for (int row = 0; row < Constants.BOARD_SIZE; row++) {
             System.out.printf(Constants.BOARD_STRING, getXOrO(state.getBoardCell(row, 0)), getXOrO(state.getBoardCell(row, 1)), getXOrO(state.getBoardCell(row, 2)));
+            System.out.println();
             System.out.println(Constants.DIVIDER_STRING);
         }
     }

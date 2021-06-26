8fa4cee99b2b1ef88507bcd38d11e10938a53fb1
Roger Jaffe <rogerjaffe@gmail.com>
2021-06-26 10:17:47 -0700

    Syntax errors fixed

--- tictactoe/EventLoop.java
+++ tictactoe/EventLoop.java
@@ -18,19 +18,19 @@ public class EventLoop {
         state.setGameState(Constants.GET_O_NAME);
     
       } else if (gameState == Constants.GET_O_NAME) {
-        state.setYName(ui.promptForName(Constants.O));
+        state.setOName(ui.promptForName(Constants.O));
         state.setGameState(Constants.GET_X_MOVE);
     
       } else if (gameState == Constants.GET_X_MOVE) {
-        row = ui.getMoveRow(state.getWhoseMove());
-        col = ui.getMoveCol(state.getWhoseMove());
+        row = ui.getMoveRow(state.getWhoseMove(), state.getXName(), state.getOName());
+        col = ui.getMoveCol(state.getWhoseMove(), state.getXName(), state.getOName());
         if (ui.isLegalMove(state, row, col)) {
           state.setGameState(Constants.MAKE_MOVE);
         }
 
       } else if (gameState == Constants.GET_O_MOVE) {
-        row = ui.getMoveRow(state.getWhoseMove());
-        col = ui.getMoveCol(state.getWhoseMove());
+        row = ui.getMoveRow(state.getWhoseMove(), state.getXName(), state.getOName());
+        col = ui.getMoveCol(state.getWhoseMove(), state.getXName(), state.getOName());
         if (ui.isLegalMove(state, row, col)) {
           state.setGameState(Constants.MAKE_MOVE);
         }
@@ -53,7 +53,7 @@ public class EventLoop {
 
       } else if (gameState == Constants.CHECK_IF_TIE) {
         if (state.isTie()) {
-          ui.printTie();
+          ui.printTieGame();
           state.setGameState(Constants.GAME_OVER);
         } else {
           state.setWhoseMove(state.getWhoseMove() * -1);
